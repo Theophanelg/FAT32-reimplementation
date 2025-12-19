63fd2acc05b15ca8c544d3b8cb1b4afaef0b81fc
tl <tl>
2025-12-19 17:57:37 +0100

    création du binaire rust autoporté

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// Cette fonction sera appeler à chaque panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// Désactive la décoration du nom de la fonction
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop{}
 }
